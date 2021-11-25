37f875783b45d1f14d875f728c312f65abc58507
lunarwhite <dwcn22@outlook.com>
2021-11-25 11:17:39 +0800

    define item

--- hello/hello/items.py
+++ hello/hello/items.py
@@ -8,5 +8,6 @@ import scrapy
 
 class HelloItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
     pass
