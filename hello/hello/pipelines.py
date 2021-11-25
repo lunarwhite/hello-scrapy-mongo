a8bfaae343b8183bd15dd5fdd01525d1abd330b2
lunarwhite <dwcn22@outlook.com>
2021-11-25 11:18:23 +0800

    use mongodb pipeline

--- hello/hello/pipelines.py
+++ hello/hello/pipelines.py
@@ -5,9 +5,28 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import pymongo
+from scrapy.exceptions import DropItem
+from scrapy.utils.project import get_project_settings
 
 
-class HelloPipeline:
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        settings = get_project_settings()
+        connection = pymongo.MongoClient(
+            settings['MONGODB_HOST'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
     def process_item(self, item, spider):
+        isValid = True
+        for data in item:
+            if not data:
+                isValid = False
+                raise DropItem("Missing {0}!".format(data))
+        if isValid:
+            self.collection.insert(dict(item))
         return item
